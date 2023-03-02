aaeba46494e60ed0a8b06d84af5c080ff9bd7045
hoanglechau <hoangchau612@gmail.com>
2023-03-02 20:27:48 +0700

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Item = ({ path, index, setFunction }) => {
   return (
-    <div>
+    <>
       <img
         src={`/character/${path}/${index}.png`}
         alt="Item"
         onClick={() => setFunction(index)}
         className="item"
       />
-    </div>
+    </>
   );
 };
 
