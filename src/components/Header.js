a71bdf8a060a9d28d64edcb2d123bc74f3b14505
hoanglechau <hoangchau612@gmail.com>
2023-03-02 19:09:00 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-title">CHARACTER BUILDER</div>
+      <div className="header-divider"></div>
+    </div>
+  );
+};
+
+export default Header;
