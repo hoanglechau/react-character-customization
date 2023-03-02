27cb05a9c9ee4ab19a7feb77adc2765552e4c0bb
hoanglechau <hoangchau612@gmail.com>
2023-03-02 22:11:03 +0700

    Update Avatar.js

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AvatarParts from "./AvatarParts";
+import AvatarPart from "./AvatarPart";
 
 const Avatar = (
   {
@@ -21,20 +21,20 @@ const Avatar = (
 ) => {
   return (
     <div className="avatar">
-      <AvatarParts path="body" index={body} zIndex={0} />
-      <AvatarParts path="hair" index={hair} zIndex={6} />
-      <AvatarParts path="eyes" index={eyes} zIndex={1} />
-      <AvatarParts path="eyebrows" index={eyebrows} zIndex={4} />
-      <AvatarParts path="noses" index={0} zIndex={4} />
-      <AvatarParts path="mouths" index={mouth} zIndex={4} />
-      <AvatarParts path="facial_hair" index={facialHair} zIndex={7} />
-      <AvatarParts path="accessories/glasses" index={glasses} zIndex={8} />
-      <AvatarParts path="accessories/earrings" index={earrings} zIndex={8} />
-      <AvatarParts path="accessories/necklace" index={necklace} zIndex={5} />
-      <AvatarParts path="accessories/hats" index={hats} zIndex={9} />
-      <AvatarParts path="clothes/layer_1" index={clothing1} zIndex={2} />
-      <AvatarParts path="clothes/layer_2" index={clothing2} zIndex={3} />
-      <AvatarParts path="clothes/layer_3" index={clothing3} zIndex={4} />
+      <AvatarPart path="body" index={body} zIndex={0} />
+      <AvatarPart path="hair" index={hair} zIndex={6} />
+      <AvatarPart path="eyes" index={eyes} zIndex={1} />
+      <AvatarPart path="eyebrows" index={eyebrows} zIndex={4} />
+      <AvatarPart path="noses" index={0} zIndex={4} />
+      <AvatarPart path="mouths" index={mouth} zIndex={4} />
+      <AvatarPart path="facial_hair" index={facialHair} zIndex={7} />
+      <AvatarPart path="accessories/glasses" index={glasses} zIndex={8} />
+      <AvatarPart path="accessories/earrings" index={earrings} zIndex={8} />
+      <AvatarPart path="accessories/necklace" index={necklace} zIndex={5} />
+      <AvatarPart path="accessories/hats" index={hats} zIndex={9} />
+      <AvatarPart path="clothes/layer_1" index={clothing1} zIndex={2} />
+      <AvatarPart path="clothes/layer_2" index={clothing2} zIndex={3} />
+      <AvatarPart path="clothes/layer_3" index={clothing3} zIndex={4} />
     </div>
   );
 };
